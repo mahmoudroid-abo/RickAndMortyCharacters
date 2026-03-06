dcfd551d7935bdfb98538cd3fcb62595be017f02
Mahmoud Ali <maboelenen555@gmail.com>
2026-03-06 02:31:57 +0200

    add interceptor logic

--- app/src/main/java/com/mahmoudroid/rickmorty/data/RetrofitInstance.kt
+++ app/src/main/java/com/mahmoudroid/rickmorty/data/RetrofitInstance.kt
@@ -2,16 +2,29 @@ package com.mahmoudroid.rickmorty.data
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 class RetrofitInstance {
 
     companion object {
-        val base_url = "https://rickandmortyapi.com/api/"
+
+        private const val BASE_URL = "https://rickandmortyapi.com/api/"
+
         fun getRetrofitInstance(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(base_url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         }
     }
 }
