adc1ea3a5447c5df959957b93150736d3d1970b1
Mahmoud Ali <maboelenen555@gmail.com>
2026-03-06 02:47:31 +0200

    refactor files structure

--- app/src/main/java/com/mahmoudroid/rickmorty/ui/CharacterDetails.kt
+++
@@ -1,7 +0,0 @@
-package com.mahmoudroid.rickmorty.ui
-
-import androidx.fragment.app.Fragment
-import com.mahmoudroid.rickmorty.R
-
-class CharacterDetails  : Fragment(R.layout.details_character){
-}
---
+++ app/src/main/java/com/mahmoudroid/rickmorty/ui/details/CharacterDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.mahmoudroid.rickmorty.ui.details
+
+import androidx.fragment.app.Fragment
+import com.mahmoudroid.rickmorty.R
+
+class CharacterDetailsFragment  : Fragment(R.layout.details_character){
+}
